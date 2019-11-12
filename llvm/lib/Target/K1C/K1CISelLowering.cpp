7089437e9252e4be951bc1e64d3c00a21aa27022
Laurent Thévenoux <lthevenoux@kalray.eu>
2019-11-12 16:13:45 +0000

    [k1c] Reminder for floating point types
    Summary: Support for reminder operation with floating point types.
    
    Test Plan: CI
    
    Reviewers: O81 llvm-project!
    
    Differential Revision: https://phab.kalray.eu/D2619

--- llvm/lib/Target/K1C/K1CISelLowering.cpp
+++ llvm/lib/Target/K1C/K1CISelLowering.cpp
@@ -166,6 +166,7 @@ K1CTargetLowering::K1CTargetLowering(const TargetMachine &TM,
     setOperationAction(ISD::BR_CC, VT, Expand);
 
     setOperationAction(ISD::FDIV, VT, VT == MVT::f16 ? Promote : Expand);
+    setOperationAction(ISD::FREM, VT, VT == MVT::f16 ? Promote : Expand);
     setOperationAction(ISD::FSQRT, VT, VT == MVT::f16 ? Promote : Expand);
 
     setOperationAction(ISD::SELECT_CC, VT, Expand);

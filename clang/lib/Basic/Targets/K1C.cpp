eff40d4e11177842ecfa49551319659e8edfc6d3
Laurent Thévenoux <lthevenoux@kalray.eu>
2019-11-22 23:06:05 +0100

    [k1c] Add getGCCRegNames for assembly parsing
    Summary: Add assembly register names for assembly parsing.
    
    Test Plan: CI
    
    Reviewers: O81 llvm-project!
    
    Differential Revision: https://phab.kalray.eu/D2607

--- clang/lib/Basic/Targets/K1C.cpp
+++ clang/lib/Basic/Targets/K1C.cpp
@@ -20,6 +20,25 @@
 using namespace clang;
 using namespace clang::targets;
 
+ArrayRef<const char *> K1CTargetInfo::getGCCRegNames() const {
+  static const char *const GCCRegNames[] = {
+      "$r0",   "$r1",   "$r2",   "$r3",   "$r4",   "$r5",   "$r6",   "$r7",
+      "$r8",   "$r9",   "$r10",  "$r11",  "$r12",  "$r13",  "$r14",  "$r15",
+      "$r16",  "$r17",  "$r18",  "$r19",  "$r20",  "$r21",  "$r22",  "$r23",
+      "$r24",  "$r25",  "$r26",  "$r27",  "$r28",  "$r29",  "$r30",  "$r31",
+      "$r32",  "$r33",  "$r34",  "$r35",  "$r36",  "$r37",  "$r38",  "$r39",
+      "$r40",  "$r41",  "$r42",  "$r43",  "$r44",  "$r45",  "$r46",  "$r47",
+      "$r48",  "$r49",  "$r50",  "$r51",  "$r52",  "$r53",  "$r54",  "$r55",
+      "$r56",  "$r57",  "$r58",  "$r59",  "$r60",  "$r61",  "$r62",  "$r63",
+      "$pc",   "$ps",   "$spc",  "$sps",  "$sspc", "$ssps", "$sr3",  "$sr4",
+      "$cs",   "$ra",   "$pcr",  "$ls",   "$le",   "$lc",   "$ea",   "$ev",
+      "$res0", "$res1", "$res2", "$res3", "$ev4",  "$men",  "$pmsa", "$aepc",
+      "$pm0",  "$pm1",  "$pm2",  "$pm3",  "$pmc",  "$sr0",  "$sr1",  "$sr2",
+      "$t0v",  "$t1v",  "$tcr",  "$wdc",  "$wdr",  "$ill",  "$ile",  "$ilh",
+      "$mmc",  "$tel",  "$teh",  "$dv",   "$oce0", "$oce1", "$ocec", "$ocea",
+      "$es",   "$ilr",  "$ws",   "$mes"};
+  return llvm::makeArrayRef(GCCRegNames);
+}
 void K1CTargetInfo::getTargetDefines(const LangOptions &Opts,
                                      MacroBuilder &Builder) const {
   DefineStd(Builder, "unix", Opts);

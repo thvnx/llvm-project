38b57e95f042f88a05f709220b94b154ed77d544
Laurent Thévenoux <lthevenoux@kalray.eu>
2020-03-06 12:37:54 +0000

    [kvx] Support select_cc with f16 values
    Summary:
    This patch adds patterns to match select_cc for f16 values.
    
    ref T12105
    
    Test Plan: New codegen tests (select_cc.c).
    
    Reviewers: O81 llvm-coolidge!
    
    Maniphest Tasks: T12105
    
    Differential Revision: https://phab.kalray.eu/D4486

--- llvm/test/CodeGen/KVX/select_cc.c
+++ llvm/test/CodeGen/KVX/select_cc.c
@@ -13,3 +13,26 @@ int f_select_cc_i64(long c, long c2, int a, int b) {
   // CHECK: cmoved.wnez
   // CHECK: ret
 }
+
+typedef _Float16 __attribute__((__vector_size__(8))) v4f16;
+v4f16 f_Select32PAT(v4f16 x, v4f16 y) {
+  v4f16 z;
+  z[0] = x[0] < y[0] ? y[0] : x[0];
+  z[1] = x[1] < y[1] ? y[1] : x[1];
+  z[2] = x[2] < y[2] ? y[2] : x[2];
+  z[3] = x[3] < y[3] ? y[3] : x[3];
+  return z;
+  // CHECK: f_Select32PAT
+  // CHECK: cmoved.wnez
+  // CHECK: cmoved.wnez
+  // CHECK: cmoved.wnez
+  // CHECK: cmoved.wnez
+  // CHECK: ret
+}
+
+_Float16 f_Select64PAT(long c, _Float16 a, _Float16 b) {
+  return c == 0 ? a : b;
+  // CHECK: f_Select64PAT
+  // CHECK: cmoved.deqz
+  // CHECK: ret
+}

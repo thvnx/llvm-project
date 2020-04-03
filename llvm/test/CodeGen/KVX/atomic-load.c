1ce33f7af6049c09a8b00dcc7bf2c94611077438
Laurent Thévenoux <lthevenoux@kalray.eu>
2020-04-03 11:35:27 +0200

    [kvx] Add pattern for extending atomic load of 16 bits to 32 bits
    Summary: ref T12274
    
    Test Plan: New codegen tests (atomic-load.c).
    
    Reviewers: O81 llvm-coolidge!
    
    Maniphest Tasks: T12274
    
    Differential Revision: https://phab.kalray.eu/D5145

--- llvm/test/CodeGen/KVX/atomic-load.c
+++ llvm/test/CodeGen/KVX/atomic-load.c
@@ -8,8 +8,26 @@ int load8to32(_Atomic char *p) {
   // CHECK: lbz.u $r0 = 0[$r0]
 }
 
+short load8to16(_Atomic char *p) {
+  return atomic_load(p);
+  // CHECK-LABEL: load8to16
+  // CHECK: lbz.u $r0 = 0[$r0]
+}
+
 long load8to64(_Atomic char *p) {
   return atomic_load(p);
   // CHECK-LABEL: load8to64
   // CHECK: lbz.u $r0 = 0[$r0]
+}
+
+int load16to32(_Atomic short *p) {
+  return atomic_load(p);
+  // CHECK-LABEL: load16to32
+  // CHECK: lhz.u $r0 = 0[$r0]
+}
+
+long load16to64(_Atomic short *p) {
+  return atomic_load(p);
+  // CHECK-LABEL: load16to64
+  // CHECK: lhz.u $r0 = 0[$r0]
 }
